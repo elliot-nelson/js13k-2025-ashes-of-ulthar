5ed9acb49709099b94cea3086b8864de02468d99
Elliot Nelson <elliot.nelson@wbd.com>
2025-09-08 11:13:43 -0400

    :bulb: Update comments

--- src/js/Viewport.js
+++ src/js/Viewport.js
@@ -11,7 +11,7 @@ export const Viewport = {
     init() {
         Viewport.canvas = document.getElementById('canvas');
         Viewport.ctx = Viewport.canvas.getContext('2d');
-        Viewport.resize(true);
+        Viewport.resize();
     },
 
     // Unlike past years, this canvas does not build in "buffers" to the gameplay canvas,
@@ -20,7 +20,7 @@ export const Viewport = {
     //
     // This may or may not be appropriate for every game, but it works for this one, and is
     // a little less code :).
-    resize(force) {
+    resize() {
         let dpr = window.devicePixelRatio,
             clientWidth = Viewport.canvas.clientWidth,
             clientHeight = Viewport.canvas.clientHeight;
@@ -35,13 +35,23 @@ export const Viewport = {
             Viewport.clientWidth = clientWidth;
             Viewport.clientHeight = clientHeight;
 
+            // What is this?
+            //
+            // Basically, imagine if the browser happens to be 640x360 pixels. If you set
+            // your canvas to be size "320x180", and render our pixel art at that size, it
+            // will be UPSCALED to 640x360, causing blurriness that cannot be avoided.
+            //
+            // Instead, we want to render AT LEAST at the actual pixel size. And in fact,
+            // if the actual monitor pixels are 2x or 4x pixels like many modern screens,
+            // we want to render at that scale and DOWNSCALE if necessary.
+            //
+            // TLDR: We set the CANVAS size to something that is AT LEAST AS LARGE as
+            // (ratio between our desired size and browser pixels) multipled by DPR.
             let pixelScale = Math.ceil(clientWidth * dpr / TARGET_GAME_WIDTH);
 
             Viewport.canvas.width = TARGET_GAME_WIDTH * pixelScale;
             Viewport.canvas.height = TARGET_GAME_HEIGHT * pixelScale;
             Viewport.scale = pixelScale;
-
-            console.log(Viewport.width, Viewport.height, Viewport.clientWidth, Viewport.clientHeight, pixelScale);
 
             // Make sure to set this every time the canvas changes size.
             Viewport.ctx.imageSmoothingEnabled = false;
